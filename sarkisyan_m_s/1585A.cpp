adb31f313a142e4426fa38d00b3d73ac31e116f9
MariaSark <113974261+MariaSark@users.noreply.github.com>
2022-10-11 11:59:25 +0300

    Create 1585A.cpp

---
+++ sarkisyan_m_s/1585A.cpp
@@ -0,0 +1,21 @@
+// Example program
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int n = 0;
+    int i = 0;
+    int k = 0;
+    int ai = 0;
+ cin >> ai; 
+ for (int ai = 0 ; ai = 1){
+     cin >> n >> i >> k ;
+     if ((i-1) && (i) && (i>1)){
+         count << (k+5) << endl;
+     }
+     else {
+         count << "0" << endl;
+     }
+ }
+}
